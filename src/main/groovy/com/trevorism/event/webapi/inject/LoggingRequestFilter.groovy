c4f61c825bf2042ec3a6116d0e26991430a89fb4
Trevor Brooks <trevorism@gmail.com>
2017-09-12 11:10:50 -0400

    Change the logger to just log the correlationId

--- src/main/groovy/com/trevorism/event/webapi/inject/LoggingRequestFilter.groovy
+++ src/main/groovy/com/trevorism/event/webapi/inject/LoggingRequestFilter.groovy
@@ -16,10 +16,6 @@ class LoggingRequestFilter implements ContainerRequestFilter{
     @Override
     void filter(ContainerRequestContext requestContext) throws IOException {
         String correlationId = requestContext.getHeaderString(HeadersHttpClient.CORRELATION_ID_HEADER_KEY)
-        String output = ""
-        if(correlationId)
-            output = "${correlationId}: "
-
-        log.info("${output}Received a ${requestContext.getMethod()} request at ${requestContext.requestUri}")
+        log.info("CorrelationId: ${correlationId}")
     }
 }
