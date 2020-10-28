d276158cb461e7cece482327762ccd6ff740f53c
trevorism <trevorism@gmail.com>
2020-10-27 22:23:48 -0400

    Remove unnecessary logging

--- src/main/groovy/com/trevorism/event/webapi/controller/EventListenerController.groovy
+++ src/main/groovy/com/trevorism/event/webapi/controller/EventListenerController.groovy
@@ -9,7 +9,6 @@ import javax.ws.rs.POST
 import javax.ws.rs.Path
 import javax.ws.rs.PathParam
 import javax.ws.rs.core.MediaType
-import java.util.logging.Logger
 
 /**
  * @author tbrooks
@@ -18,13 +17,10 @@ import java.util.logging.Logger
 @Path("/_ah/push-handlers")
 class EventListenerController {
 
-
-    private static final Logger log = Logger.getLogger(EventListenerController.class.name)
-
     @POST
     @Path("store_{topic}")
     @Consumes(MediaType.APPLICATION_JSON)
-    void store(@PathParam("topic") String topic, Map<String, Object> data){
+    void store(@PathParam("topic") String topic, Map<String, Object> data) {
         EventData eventData = EventData.createFromMap(data)
         EventHandlerFactory.build("store").performAction(eventData)
     }
@@ -32,15 +28,9 @@ class EventListenerController {
     @POST
     @Path("handle_{topic}")
     @Consumes(MediaType.APPLICATION_JSON)
-    void handle(@PathParam("topic") String topic, Map<String, Object> data){
-        StringBuilder logBuilder = new StringBuilder()
-        data.each {k,v ->
-            logBuilder << "$k:$v\n"
-        }
-        log.warning(logBuilder.toString())
+    void handle(@PathParam("topic") String topic, Map<String, Object> data) {
         EventData eventData = EventData.createFromMap(data)
         EventHandlerFactory.build(topic).performAction(eventData)
-
     }
 
 }
